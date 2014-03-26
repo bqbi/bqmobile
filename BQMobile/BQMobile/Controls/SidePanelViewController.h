23c8305794301eff1c3b39ba7bc4611b281800a7
wangqzh <27696830@qq.com>
2014-03-26 22:46:55 +0800

    增加实现侧边栏具体类定义
    侧边栏具体单元实现类以及数据代理定义

---
+++ BQMobile/BQMobile/Controls/SidePanelViewController.h
@@ -0,0 +1,14 @@
+//
+//  SidePanelViewController.h
+//  LeftDragPanelDemo
+//
+//  Created by mac on 2014-02-26.
+//  Copyright (c) 2014年 QZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TableViewDataSourceDelegate.h"
+
+@interface SidePanelViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic,retain) id<TableViewDataSourceDelegate> delegate;
+@end
