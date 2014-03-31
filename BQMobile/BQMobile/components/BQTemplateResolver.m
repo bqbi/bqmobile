63cd7b4b7f7edb598ac9b4d48194da77b5154a83
Peter Pan <panbasten@126.com>
2014-03-31 08:56:09 +0800

    模板解析

---
+++ BQMobile/BQMobile/components/BQTemplateResolver.m
@@ -0,0 +1,20 @@
+//
+//  BQTemplateResolver.m
+//  BQMobile
+//
+//  Created by 潘 巍 on 14-3-30.
+//  Copyright (c) 2014年 yonyou. All rights reserved.
+//
+
+#import "BQTemplateResolver.h"
+
+#import "GDataXMLNode.h"
+#import "XMLUtils.h"
+
+@implementation BQTemplateResolver
+
++ (void) resolverSubNode:(GDataXMLElement*)node withParentComponent:(UIView*)parentView {
+//    [node children]
+}
+
+@end
