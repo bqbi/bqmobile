41967ec70377ba5e77a1379c662badd9afb610f8
Peter Pan <panbasten@126.com>
2014-03-31 11:31:08 +0800

    组件模块框架

---
+++ BQMobile/BQMobile/components/BQComponentFactory.h
@@ -0,0 +1,13 @@
+//
+//  BQComponentFactory.h
+//  BQMobile
+//
+//  Created by 潘 巍 on 14-3-31.
+//  Copyright (c) 2014年 yonyou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BQComponentFactory : NSObject
+
+@end
