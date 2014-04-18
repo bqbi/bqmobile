01f5804ade9ce4e786b6a2c2834889097626e03b
Peter Pan <panbasten@126.com>
2014-04-18 14:48:16 +0800

    添加分析主题基类

---
+++ BQMobile/BQMobile/ipad/BQIpadAnalysisTopicController.m
@@ -0,0 +1,21 @@
+//
+//  BQAnalysisTopicController.m
+//  BQMobile
+//
+//  Created by 潘 巍 on 14-3-27.
+//  Copyright (c) 2014年 yonyou. All rights reserved.
+//
+
+#import "BQIpadAnalysisTopicController.h"
+
+
+@interface BQIpadAnalysisTopicController ()
+
+@end
+
+
+@implementation BQIpadAnalysisTopicController
+
+
+
+@end
